e455cf08362aaaa6a022d63bda58563e04d5be83
sudeepnn <sudeep2002naik@gmail.com>
2023-05-08 16:10:23 +0530

    login and signup page

---
+++ app/src/main/java/com/example/venuebooking/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.venuebooking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void getstarted(View view){
+        setContentView(R.layout.loginpage);
+    }
+    public void loigin(View view){
+        setContentView(R.layout.signuppage);
+    }
+
+
+}
